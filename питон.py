9e538391fa7a2d66e4772f25e77213a433a4b02d
lizaakupt <156932695+lizaakupt@users.noreply.github.com>
2024-03-03 21:40:59 +0300

    домашняя работа на 06.03.24

---
+++ питон.py
@@ -0,0 +1,35 @@
+import pygame
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+
+x = 100
+y = 50
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+         exit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= 3
+    elif keys[pygame.K_RIGHT]:
+        x += 3
+    elif keys[pygame.K_UP]:
+        y -= 3
+    elif keys[pygame.K_DOWN]:
+        y += 3
+    else:
+        x = y = 250
+
+    color = (0, 255, 0)
+    if x > 400 or x < 100 or y < 100 or y > 400:
+        color = (255, 0, 0)
+    else:
+        color = (0, 255, 0)
+
+    win.fill((0, 155, 45))
+
+    pygame.draw.circle(win, (0, 255, 0), (x, y), radius= 10)
+    pygame.time.delay(10)
+    pygame.display.update()
