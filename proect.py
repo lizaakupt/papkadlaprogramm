d40b6add9de479bc5283c947efc1721e911e6142
lizaakupt <156932695+lizaakupt@users.noreply.github.com>
2024-05-05 14:39:03 +0300

    проект по программированию

--- proect.py
+++ proect.py
@@ -10,24 +10,39 @@ class Board:
         self.surface = surface
         self.x = 225
         self.y = 225
-        self.start_pos = (175, 225)
-        self.end_pos = (275, 225)
+        self.x1 = 175
+        self.y1 = 225
+        self.x2 = 275
+        self.y2 = 225
+
 
     def draw_circle(self, radius):
         self.x += 1
         self.y += 1
+
         pygame.draw.circle(self.surface, 'white', (self.x, self.y), radius)
 
     def draw_line(self, width):
-        pygame.draw.line(win,'white', self.start_pos, self.end_pos, 3)
+        pygame.draw.line(win,'white', (self.x1, self.y1), (self.x2, self.y2), 3)
+
+
     def move_by_keys(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.start_pos -= 3
-            self.end_pos -= 3
+            self.x1 -= 3
+            self.x2 -= 3
+
         elif keys[pygame.K_RIGHT]:
-            self.end_pos += 3
-            self.start_pos += 3
+            self.x1 += 3
+            self.x2 += 3
+
+    def updated(self):
+        if self.x > 450 or self.y > 450:
+            self.x = -1
+            self.y = -1
+        elif self.x < 0 or self.y < 0:
+            self.y = +1
+            self.x = +1
 
 
 
@@ -36,12 +51,13 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+
+
     win.fill(('black'))
     figure.draw_circle(7)
     figure.move_by_keys()
     figure.draw_line(10)
+    figure.updated()
 
-
-
-    pygame.time.delay(10)
+    pygame.time.delay(5)
     pygame.display.update()
